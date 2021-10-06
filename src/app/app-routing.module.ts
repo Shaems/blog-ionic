bf628be98040fd76912d10ca6cfa60339f907acb
Shaems <shaelmaximovich@gmail.com>
2021-10-06 15:31:32 -0300

    Detail post Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,10 @@ const routes: Routes = [
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: 'post',
+    loadChildren: () => import('./detail-post/detail-post.module').then( m => m.DetailPostPageModule)
   }
+
 
 ];
 @NgModule({
